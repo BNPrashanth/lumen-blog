24d81e9545a6d82cd3d756887cfa1168cb95dc83
BNPrashanth <bnprashanth256@gmail.com>
2019-05-07 14:54:11 +0530

    POST model and controller created

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function createPost(Request $request)
+    {
+        $post = post::create($request->all());
+        return response()->json($post);
+    }
+
+    public function updatePost(Request $request, $id)
+    {
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->views = $request->input('views');
+        $post->save();
+
+        return response()->json($post);
+    }
+
+    public function deletePost($id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+
+        return response()->json('Removed successfully.');
+    }
+
+    public function index()
+    {
+        $post = Post::all();
+
+        return response()->json($post);
+    }
+}
